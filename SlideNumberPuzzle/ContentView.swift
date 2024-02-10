855610ed748b059960944f5daa36bddcd2e7df40
6410615030 <6410615030@student.tu.ac.th>
2024-02-10 16:29:02 +0700

    Initial Commit

---
+++ SlideNumberPuzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlideNumberPuzzle
+//
+//  Created by Babypowder on 10/2/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
